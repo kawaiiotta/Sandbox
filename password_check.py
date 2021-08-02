15789bac549c243e4362148f23d1ce980ab59edd
kawaiiotta <80732113+kawaiiotta@users.noreply.github.com>
2021-08-02 19:56:22 +0800

    Adds a list_file function to list all files

--- password_check.py
+++ password_check.py
@@ -1,3 +1,11 @@
 """
 This code will Error check a password
 """
+password = str(input("Set new password: "))
+while not len(password) > 6:
+    print("Password is not long enough")
+    password = str(input("Set new password: "))
+
+for char in password:
+    print("*", end="")
+
